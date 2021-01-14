8311512e24539d69042587e2f22fcc9590946d4d
Thiago <thiago_rocha17@hotmail.com>
2021-01-14 18:10:32 -0300

    Exercise done: 01 - Comparison operators, strict equality

---
+++ JavaScript Basics/01 - Comparison operators, strict equality.js
@@ -0,0 +1,11 @@
+//Inputs
+let input1 = '10';
+let input2 = '10';
+
+//This function return true if the two values are equal and of the same type
+function strictEquality(a,b) {
+    return a === b;
+}
+
+//Output
+console.log(strictEquality(input1,input2));
