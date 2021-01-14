a0fc23963ecdc548a633d62eb69ab41a3c2d7dd4
Thiago <thiago_rocha17@hotmail.com>
2021-01-14 18:22:41 -0300

    Exercise done: 03 - Check if a number is even

---
+++ JavaScript Basics/03 - Check if a number is even.js
@@ -0,0 +1,14 @@
+// Write a function that takes a number as argument
+// If the number is even, return true
+// Otherwise, return false
+  
+//Input
+let number = 10
+
+//Function
+function myFunction(n) {
+   return n % 2 == 0;
+}
+
+//Output
+console.log(myFunction(number));
